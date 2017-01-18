44759c1f1aab0d0f769f62492343e0f06efc4993
luke lincoln <lukes611@gmail.com>
2017-01-18 19:41:06 +1000

    finished adding final module tutorials

--- examples/Wireless tx & rx/receiver/receiverTutorial.ino
+++ examples/Wireless tx & rx/receiver/receiverTutorial.ino
@@ -1,25 +1,19 @@
-//Connect Tx on digital pin 12
+//Connect Rx on digital pin 12
 #include <VirtualWire.h>
 void setup()
 {
-vw_set_ptt_inverted(true); //required for receiver
-vw_set_rx_pin(12);
-vw_setup(4000);  //set bits per second
-pinMode(13, OUTPUT);
-vw_rx_start(); //start the receiver
+	vw_set_ptt_inverted(true); //required for receiver
+	vw_set_rx_pin(12);
+	vw_setup(4000);  //set bits per second
+	vw_rx_start(); //start the receiver
 
-//setup serial output
-Serial.begin(9600);
+	Serial.begin(9600);
 }
 
 void loop()
 {
-uint8_t str[VW_MAX_MESSAGE_LEN];
-uint8_t len;
-
-//if message available: put it in str and set stringLength to the 
-//length of the string
-
-if (vw_get_message(str, &len)) Serial.println(str);
-	
+	uint8_t str[VW_MAX_MESSAGE_LEN];
+	uint8_t len;
+	if (vw_get_message(str, &len))
+		Serial.println(str);
 }
--- examples/Wireless tx & rx/transmitter/transmitterTutorial.ino
+++ examples/Wireless tx & rx/transmitter/transmitterTutorial.ino
@@ -3,16 +3,15 @@
 
 void setup()
 {
-pinMode(13,OUTPUT);
-vw_set_ptt_inverted(true); //
-vw_set_tx_pin(12);
-vw_setup(4000);// speed of data transfer Kbps
+	vw_set_ptt_inverted(true); //
+	vw_set_tx_pin(12);
+	vw_setup(4000);// speed of data transfer Kbps
 }
 
 void loop()
 {
-char msg = "hello there";
-vw_send((uint8_t*)msg, strlen(msg));
-vw_wait_tx(); // wait until the message is sent
-delay(2000);
+	char * msg = "hello there";
+	vw_send((uint8_t*)msg, strlen(msg));
+	vw_wait_tx(); // wait until the message is sent
+	delay(2000);
 }
